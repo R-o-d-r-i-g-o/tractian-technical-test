e4ed04ccf62bd0f4434643699b86f2a22acf48cf
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-04-11 21:17:07 -0300

    feat: add color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    // Note: don't allow extend colors.
+    colors: {
+      primary: '#17192D',
+      secondary: '#023B78',
+      selection: '#2188FF',
+      accent: '#ffc107',
+      success: '#52C41A',
+      danger: '#ED3833',
+      // TODO: implemente "warning" & "info" colors.
+    },
   },
   plugins: [],
 };
