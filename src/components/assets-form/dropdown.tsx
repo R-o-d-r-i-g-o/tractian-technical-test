68456b6285b977029324ca309e65a6dda1b5b3de
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-04-13 23:33:26 -0300

    feat: add dropdown complete tree example

--- src/components/assets-form/dropdown.tsx
+++ src/components/assets-form/dropdown.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import React, { memo, useState, useEffect } from 'react';
 
-import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa6";
 
 import ApexAssets from '../../../public/mock/apex-unit/assets.json'
 import ApexLocations from '../../../public/mock/apex-unit/locations.json'
+
+const content = ApexLocations // ApexAssets.concat(ApexLocations)
 
 type Props = {
     search: string;
@@ -15,10 +17,18 @@ type Props = {
 const getLocations = (search: string) => {
   search = search?.toLowerCase()
 
-  return ApexLocations.filter(
-    item => item.parentId == null
+  return content.filter(
+    item => item.parentId == null // && item.
     && (search === "" || item.name.toLowerCase().includes(search))
   )
+}
+
+const getAssociatedLocations = (id: string) => {
+
+  return content.filter(item => {
+
+    return item.parentId == "60fc7c9e86cd05001d22b4d5"
+  })
 }
 
 const Dropdown = ({ search }: Props) => {
@@ -31,17 +41,51 @@ const Dropdown = ({ search }: Props) => {
   return (
     <>
       {data.map((item) =>
-        <div
-          key={item.id}
-          className='flex gap-2 items-center text-sm'
-        >
-          <FaAngleDown size={12} />
-          <Image src='/icons/location.svg' alt='' height={14} width={14} />
-          {item.name}
-        </div>
+        <DropdownItem key={item.id} {...item} />
       )}
     </>
   );
-} 
+}
+
+type DropdownItemProps = {
+  id: string;
+  name: string;
+}
+
+const DropdownItem = ({ id, name }: DropdownItemProps) => {
+  const [showItems, setShowItems] = useState(false)
+  const [subitems, setSubitems] = useState<typeof ApexLocations>([])
+
+  const handleShowDetails = () => {
+    setShowItems(!showItems)
+
+    if (showItems) return;
+    setSubitems(getAssociatedLocations(id))
+  }
+
+  const buttonIcon = showItems
+    ? <FaAngleDown size={12} />
+    : <FaAngleRight size={12} />
+
+  return (
+    <div className='cursor-pointer text-sm' >
+      <div
+        onClick={handleShowDetails}
+        className='flex gap-2 items-center'
+      >
+        {buttonIcon}
+        <Image src='/icons/location.svg' alt='' height={14} width={14} />
+        {name}
+      </div>
+      {showItems &&
+        <div className='min-h-min ml-4'>
+          {subitems.map((item) =>
+            <DropdownItem key={item.id} {...item} />
+          )}
+        </div>
+      }
+    </div>
+  )
+}
 
 export default memo(Dropdown);
