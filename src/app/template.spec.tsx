8bf2b1ac43ba33ea27038b317ff3c4e4add9e99a
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-04-15 17:33:51 -0300

    feat: add test-cases setup

---
+++ src/app/template.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Template from './template';
+
+describe('Template', () => {
+  it('renderiza o componente Template corretamente', () => {
+    // render(<Template>{'Hello world!'}</Template>);
+    // expect(screen.getByRole('heading', { name: /header/i })).toBeDefined();
+    // console.log('teste', expect(screen.getByText(/Hello World/i)));
+    // expect(screen.getByText(/Hello World/i)).toBeUndefined();
+  });
+});
