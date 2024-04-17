c2b96577d69d56fea6f1fcde5bb338e4da45345c
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-04-17 15:15:43 -0300

    feat: add resposivity

--- src/app/@components/page-details/index.tsx
+++ src/app/@components/page-details/index.tsx
@@ -15,8 +15,8 @@ const DetailsScreen = ({ name, status }: t.Assets) => (
       <StatusView status={status} />
     </div>
     <div className="w-full min-h-min p-5">
-      <div className="flex gap-4">
-        <div className="w-1/2">
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2 flex justify-center">
           <Image
             src="https://s3-alpha-sig.figma.com/img/324a/327b/49e2fd18b176b9e2ef843bdfef57629f?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RAuFBy0lakmPqiqarb8tZPhF9GNiR77qwmRB7VEWVAdOnCJ1qNb6qxBsoXfQCds4sRx9mQ36XousSz1FOyxQ9lx-mGKAW1HDck6wnk-GDhuX5pLIXrDX-eAEMgaDkU-372PigWQlRe0TACDCN1d1IDVd0KPpyuU~e8j8nu-DiC01qqUuKU85zbFd77jh4ZvN2jv02j38ocYcMkL9eWLP1nGuwXlZOg327BLrImWQaj87vfG8uaRXv99cDEBsVuO-03JAx1bFtfIZ-mHnHuC6L11IXD3COUTFvB3uEnwYEMo0arfERktY1EunLQnRSfINkZHEoGMtbQRgOjm6~eMjFg"
             alt="equipament-frame"
@@ -25,14 +25,18 @@ const DetailsScreen = ({ name, status }: t.Assets) => (
             width={336}
           />
         </div>
-        <div className="w-1/2 pt-12 font-normal leading-6">
-          <span className="font-semibold text-base">Tipo de Equipamento</span>
-          <div className="text-dark_gray">Motor Elétrico (Trifásico)</div>
-          <hr className="text-gray h-px mt-4 mb-6" />
-          <span className="block font-semibold text-base mb-2">
-            Reponsáveis
-          </span>
-          <Collaborators alignRight users={mockCollaborators} />
+        <div className="w-full md:w-1/2 flex flex-row md:flex-col gap-4 md:gap-0 pt-12 font-normal leading-6">
+          <div className="w-1/2">
+            <span className="font-semibold text-base">Tipo de Equipamento</span>
+            <div className="text-dark_gray">Motor Elétrico (Trifásico)</div>
+          </div>
+          <hr className="text-gray h-px mt-4 mb-6 hidden md:block" />
+          <div className="w-1/2">
+            <span className="block font-semibold text-base mb-2">
+              Reponsáveis
+            </span>
+            <Collaborators alignRight users={mockCollaborators} />
+          </div>
         </div>
       </div>
       <hr className="text-gray h-px mt-4 mb-6" />
